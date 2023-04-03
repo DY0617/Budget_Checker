456e9a4b87c9612d8d8e0920811c281c64cfa69d
dongyeop <brassdong@gmail.com>
2023-04-03 16:29:53 +0900

    intercepter

---
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.config;
+
+
+import com.example.demo.common.security.auth.LoginUserArgumentResolver;
+import com.example.demo.interceptor.LoggerInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+    }
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+
+
+}
