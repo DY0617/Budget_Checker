a053b70da7e8a975abc6a73196f478e37386a02d
dongyeop <brassdong@gmail.com>
2023-04-03 14:59:48 +0900

    save test code

--- src/test/java/com/example/demo/domain/user/UserMapperTest.java
+++ src/test/java/com/example/demo/domain/user/UserMapperTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.domain.user;
 
 
+import com.example.demo.common.dto.UserDto;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,8 +10,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class UserMapperTest {
 
     @Autowired
-    UserMapper usermapper;
+    UserMapper userMapper;
 
 
+    @Test
+    void save() {
+        UserDto.Request user = new UserDto.Request();
+        user.setEmail("brassdong@gmail.com");
+        user.setPassword("ehdduq0617!");
+        user.setNickname("테스터");
+        user.setUsername("brassdong");
+
+        userMapper.save(user.toEntity());
+
+    }
 
 }
