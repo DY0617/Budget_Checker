4fce6785b57d0468263bc61acbba5956a16010e5
dongyeop <brassdong@gmail.com>
2023-04-03 14:41:58 +0900

    Create UserMapperTest.java

---
+++ src/test/java/com/example/demo/domain/user/UserMapperTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.user;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserMapperTest {
+
+    @Autowired
+    UserMapper usermapper;
+
+
+
+}
