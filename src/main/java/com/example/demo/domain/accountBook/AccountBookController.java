022011fab21e4b732dce9d97c74e70a8ae6e06cd
dongyeop <brassdong@gmail.com>
2023-04-05 19:23:58 +0900

    user

--- src/main/java/com/example/demo/domain/accountBook/AccountBookController.java
+++ src/main/java/com/example/demo/domain/accountBook/AccountBookController.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.accountBook;
 
+import com.example.demo.common.dto.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class AccountBookController {
+
+    // 사용자에게 메시지를 전달하고, 페이지를 리다이렉트 한다.
+    private String showMessageAndRedirect(final MessageDto params, Model model) {
+        model.addAttribute("params", params);
+        return "common/messageRedirect";
+    }
 
     @GetMapping("/account")
     public String openAccountMain(Model model) {
