de8886b28bac9ed3231b01aac07c790e8ceb53bf
dongyeop <brassdong@gmail.com>
2023-04-05 19:31:34 +0900

    Update AccountBookService.java

--- src/main/java/com/example/demo/domain/accountBook/AccountBookService.java
+++ src/main/java/com/example/demo/domain/accountBook/AccountBookService.java
@@ -1,4 +1,9 @@
 package com.example.demo.domain.accountBook;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AccountBookService {
 }
