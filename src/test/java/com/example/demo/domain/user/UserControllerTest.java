7fdf1fcb60a5ed8090dcd04868688cf4aa97b7f4
dongyeop <brassdong@gmail.com>
2023-04-06 17:37:53 +0900

    Update UserControllerTest.java

--- src/test/java/com/example/demo/domain/user/UserControllerTest.java
+++ src/test/java/com/example/demo/domain/user/UserControllerTest.java
@@ -1,4 +1,17 @@
 package com.example.demo.domain.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    UserService userService;
+
 }
